e33280e1bc5d910b5ae3e549fe49d9b35f87cabf
Chris T <37755722+hashratez@users.noreply.github.com>
2018-09-14 22:54:43 -0400

    This was forked from TurtleCoin Explorer

---
+++ config.js
@@ -0,0 +1,18 @@
+var api = 'http://127.0.0.1:18112';
+var donationAddress = "";
+var blockTargetInterval = 30;
+var coinUnits = 10000;
+var symbol = 'ERC';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+ "erc": [
+	["EveryCash Official Pool", "http://pool.everycash.xyz:8117"]
+ ]
+};
+
+var networkStat2 = {
+    "erc": [
+	[""]
+ ]
+};
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+        'api' => 'http://127.0.0.1:18112',
+        'blockTargetInterval' => 30,
+        'coinUnits' => 10000
+);
