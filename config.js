268bc6d81857137860800e65d72a8b74d532dc10
Chris T <37755722+hashratez@users.noreply.github.com>
2018-09-15 05:50:32 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,17 +2,17 @@ var api = 'http://127.0.0.1:18112';
 var donationAddress = "";
 var blockTargetInterval = 30;
 var coinUnits = 10000;
-var symbol = 'ERC';
+var symbol = 'ecx';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
- "erc": [
+ "ecx": [
 	["EveryCash Official Pool", "http://pool.everycash.xyz:8117"]
  ]
 };
 
 var networkStat2 = {
-    "erc": [
+    "ecx": [
 	[""]
  ]
 };
